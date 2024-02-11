2dfcfdd31f880a50d7fea3b820387227986a486c
vitalyvitmens <otm_@tut.by>
2024-02-11 15:25:29 +0300

    chore(src\App.js): Меняет BrowserRouter на  HashRouter для корректной работы роутинга на GitPages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Home, Category, Detail, NotFound } from './components'
 import styles from './app.module.css'
 
 export const App = () => {
 	return (
 		<div className={styles.App}>
-			<BrowserRouter basename="/rs-react-router-v6">
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/:category" element={<Category />} />
 					<Route path="/:category/:id" element={<Detail />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</div>
 	)
 }
